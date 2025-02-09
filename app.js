14b52d8cd55a337c9c46a9ab051b69a7010c874e
sumit kumar mandal <sumitmandal330@gmail.com>
2025-02-09 13:06:43 +0530

    logout and profile

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes')
+const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser'); 
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
-
+const authMiddleware = require('../middleware/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -19,5 +19,7 @@ router.post('/Login',[
 userController.loginUser
 )
 
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
+router.get('/logout',authMiddleware.authUser,userController.logoutUser)
 
 module.exports = router; 
